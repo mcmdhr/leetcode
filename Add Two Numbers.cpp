090324b18c150fd3995bdbd60fee04973e8a9e34
mcmdhr <623742750@qq.com>
2015-03-08 14:34:13 +0800

    第二题

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode head(0), *p = &head;
+        int t = 0;
+        while (l1 || l2 || t){
+            int x = (l1?l1->val:0) + (l2?l2->val:0) + t;
+            p = p->next = new ListNode(x%10);
+            t = x/10;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        return head.next;
+    }
+};
